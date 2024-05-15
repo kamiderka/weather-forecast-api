84f79134d33723212163e2cae012be1a86758da5
Kamil Kordecki <kkordecky@gmail.com>
2024-05-15 23:29:54 +0200

    feat: Add CORS (don't do it like this)

--- src/main.py
+++ src/main.py
@@ -1,6 +1,23 @@
 from fastapi import FastAPI
 from forecast.router import forecast_router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "https://weather-forecast-tn81.onrender.com"
+    
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(forecast_router)
